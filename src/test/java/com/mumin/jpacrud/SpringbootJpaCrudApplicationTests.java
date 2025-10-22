b0f7b8fe65980301525825dfa5ec584cb9b5c93d
mumin-pervez <pervez.ist@gmail.com>
2025-10-22 19:16:22 +0600

    Project Initialization

---
+++ src/test/java/com/mumin/jpacrud/SpringbootJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumin.jpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaCrudApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
